e9bd9275032b0eb7bca5a6d317c239272ee78393
Sergio <sergio1991hortas@gmail.com>
2024-07-08 17:33:25 +0200

    Added mapview into details garage view

--- aparcamientoszaragoza/lib/Screens/detailsGarage/detailsGarage_screen.dart
+++ aparcamientoszaragoza/lib/Screens/detailsGarage/detailsGarage_screen.dart
@@ -1,11 +1,12 @@
 import 'package:aparcamientoszaragoza/Models/garaje.dart';
 import 'package:aparcamientoszaragoza/Screens/listComments/listComments_screen.dart';
 import 'package:aparcamientoszaragoza/Values/app_models.dart';
-import 'package:aparcamientoszaragoza/main.dart';
 import 'package:aparcamientoszaragoza/widgets/Buttons.dart';
 import 'package:aparcamientoszaragoza/widgets/Spaces.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../Values/app_colors.dart';
 
@@ -20,14 +21,27 @@ class DetailsGarajePage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsGarajePage> {
+  late CameraPosition _initialCameraPosition; // State variable for camera position
+
   @override
   Widget build(BuildContext context) {
 
-    final indexPlaza = ModalRoute.of(context)!.settings.arguments as int;
+    final indexPlaza = (ModalRoute.of(context)?.settings.arguments ?? 0) as int;
     final plaza = AppModels.defaultGarajes[indexPlaza];//.get(indexPlaza);
+    _initialCameraPosition = CameraPosition(
+      target: LatLng(plaza.latitud ?? 0.0, plaza.longitud ?? 0.0), // Handle potential null values
+      zoom: 6.0, // Set zoom level
+    );
+
     return Scaffold(
-      backgroundColor: AppColors.darkBlue,
-      body: _viewInfo(plaza)
+      //backgroundColor: AppColors.darkBlue,
+      //body: _viewInfo(plaza),
+      body: Stack( // Use Stack to position map below content
+          children: [
+            _buildMap(plaza),
+            _viewInfo(plaza),
+        ]
+      )
     );
   }
 
@@ -74,8 +88,33 @@ class _DetailsPageState extends State<DetailsGarajePage> {
       ]);
   }
 
-  /*Widget _buildMap(){
-
-  }*/
+  Widget _buildMap(Garaje plaza){
+    return Scaffold( //Card
+      body: Stack( // Use Stack to position map below content
+        children: [
+          Positioned( // Position map at the bottom
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: SizedBox(
+              height: MediaQuery.sizeOf(context).height, // Adjust map height as needed
+              child: GoogleMap(
+                initialCameraPosition: _initialCameraPosition,
+                markers: { // Add a marker for the plaza location
+                  Marker(
+                    markerId: const MarkerId('plaza_marker'),
+                    position: LatLng(
+                      plaza.latitud ?? 0.0,
+                      plaza.longitud ?? 0.0,
+                    ),
+                  ),
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
